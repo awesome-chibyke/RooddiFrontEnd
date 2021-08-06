e4333594e7e16c3897b68cb11379993026610359
Agbo Chibueze <agbochibueze13@gmail.com>
2021-08-06 14:36:30 +0100

    removed login auth

--- src/redux/combineReducers.js
+++ src/redux/combineReducers.js
@@ -1,14 +1,12 @@
 import { combineReducers } from "redux";
 import UserRegistrationReducer from "./Xetima/Register/RegisterReducers";
 import UserLoginReducer from './Xetima/Login/LoginReducers';
-import AuthReducers from './Xetima/LoginAuth/AuthReducers';
 import ForgotPasswordReducers from './Xetima/ForgotPassword/ForgotPasswordReducers';
 
 
 const rootReducer = combineReducers({
   registration: UserRegistrationReducer,
   login:UserLoginReducer,
-  authentication:AuthReducers,
   forgotpassword:ForgotPasswordReducers
 });
 
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,4 @@
 export * from "./Xetima/Register/RegisterActionCreator"; //fetchUser
 export * from "./Xetima/Login/LoginActionCreator"; //fetchUser
-export * from "./Xetima/LoginAuth/AuthActionCreator"; //fetchUser
 export * from "./Xetima/ForgotPassword/ForgotPasswordActionCreator"; //fetchUser
 
