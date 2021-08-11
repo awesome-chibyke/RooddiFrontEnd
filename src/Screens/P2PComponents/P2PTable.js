7ea50bc1ba153d67695592911be1c1f88b590288
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-08-11 12:19:36 +0100

    added p2p section

---
+++ src/Screens/P2PComponents/P2PTable.js
@@ -0,0 +1,70 @@
+import {React, useState} from 'react'
+import renderTooltip from './Tootip'
+import P2PModal from '../P2PComponents/P2PModal'
+import {OverlayTrigger, Button} from 'react-bootstrap'
+
+const Table = () => {
+
+    const [modalShow, setModalShow] = useState(false);
+
+    return (
+        <>
+           
+           <div className="row">
+                <div className="col-12 col-sm-12">
+                    <table id="example" className="table mainResponsiveTable" >
+                        <thead >
+                            <tr>
+                                <th>Advertisers</th>
+                                <th>Price</th>
+                                <th>Limit/Available</th>
+                                <th>Payment</th>
+                                <th>Trade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr style={{textAlign:'center'}}>
+                                <td>
+                                    <span className="mobile-head">Advertisers</span>
+                                        ⭐Peace Praise⭐<br/>
+                                        2293 orders 97.91% completion
+                                </td>
+                                <td>
+                                    <span className="mobile-head">Price</span>501.20<sub>NGN</sub>
+                                </td>
+                                <td>
+                                    <span className="mobile-head">Limit/Available
+                                    </span>
+                                        Available 50,058.76 USDT<br/>
+                                        Limit N50,000.00 - N321,377.23
+                                </td>
+                                <td>
+                                    
+                                    <OverlayTrigger
+                                        placement="bottom"
+                                        delay={{ show: 250, hide: 400 }}
+                                        overlay={renderTooltip}
+                                    >
+                                        <span class="btn btn-def btn-smaller">Bank Transfer
+                                        </span>
+                                    </OverlayTrigger>
+                                </td>
+                                <td>
+                                    <Button variant="primary" onClick={() => setModalShow(true)}>
+                                        Buy USDT
+                                    </Button>{' '}
+                                    <P2PModal
+                                        show={modalShow}
+                                        onHide={() => setModalShow(false)}
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div> 
+        </>
+    )
+}
+
+export default Table
--- src/components/Includes/Header.jsx
+++ src/components/Includes/Header.jsx
@@ -45,7 +45,7 @@ console.log(loginData.isLogged)
 
   return (
     <>
-      <header className="top-bar dark-overlay-top">
+      <header className="top-bar white-overlay-top">
         <div className="topbar">
           <div className="container">
             <div className="row justify-content-end">
