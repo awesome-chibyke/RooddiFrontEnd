1c3a9246debc813e31f726fd90128e6d7fc13db3
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-08-19 15:14:38 +0100

    added remaining contents to the second modal

--- src/Screens/P2PComponents/Accountdetails.js
+++ src/Screens/P2PComponents/Accountdetails.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 const Account = () => {
 
     const container = {
         marginTop: '2rem',
         marginLeft: '0.2rem', 
-        border: '1px solid gray',
+        border: '1px solid #e4e6ef',
         borderRadius: '0.3rem'
     }
 
-    const fontStyles= {
+    const content = {
+        marginTop: '2rem',
+        marginLeft: '0.2rem',
+        marginBottom: '2rem',
+        borderRadius: '0.3rem',
+        backgroundColor: '#e4e6ef',
+        color: '#3a3a3a',
+        fontWeight: '400'
+    }
+
+    const fontStyles = {
         fontWeight: 'bold'
     }
+
+    const warning = {
+        background: ' orange',
+        color: 'white',
+        marginBottom: '1rem'
+    }
+   
     return (
         <>
             <div className="container">
@@ -30,7 +48,7 @@ const Account = () => {
                     </div>
                 </div>
                 <hr style={{width: '70%'}}/>
-                <div className="row" style={{marginTop: '3rem'}}>
+                <div className="row mt-3">
                     <div className="col-2"></div>
                     <div className="col-12 col-sm-3">
                         <h5 style={fontStyles}>Amount</h5>
@@ -53,11 +71,7 @@ const Account = () => {
                                 <h5 style={{marginTop: '2rem'}}>
                                     Please confrim that you have successfuly transferred the money to the seller through the following payment method.
                                 </h5>
-                                <div className="warning" style={{
-                                    background: 'orange',
-                                    color: 'white',
-                                    marginBottom: '1rem'
-                                    }}>
+                                <div className="mt-3" style={warning}>
                                     <p style={{padding: '1rem'}}>
                                         The following is the seller's payment info. Please make sure that the money is transferred from your account you own, matching your verified name. Money will NOT transfer automatically by the platform.
                                     </p>
@@ -69,7 +83,7 @@ const Account = () => {
                 <div className="row">
                     <div className="col-12 col-sm-2"></div>
                     <div className="col-12 col-sm-8">
-                        <div className="row" style={{marginTop: '2rem'}}>
+                        <div className="row mt-3">
                             <h4 style={{
                                 fontWeight: 'bold',
                                 marginTop: '1rem',
@@ -88,6 +102,49 @@ const Account = () => {
                                 <h4>First Bank</h4>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-sm-2"></div>
+                    <div className="col-12 col-sm-8 mt-3">
+                        <h3>
+                            Payment to be made 
+                            <span style={{color:'#cc8600'}}>&nbsp;00:15:00</span>
+                        </h3>
+                        <h5>
+                            Please make payments within 15 minutes, otherwise, the order will be cancelled.
+                        </h5>
+                        <div className="mt-3" style={warning}>
+                            <p style={{padding: '1rem'}}>
+                                ATTENTION! After making the fiat transfer, please the button below to inform the seller to receive payment, fail to do so will result in automatically cancellation of order and potentialy loss of all your assets. 
+                            </p>
+                        </div>
+                            <div className="row" style={{display: 'flex', justifyContent: 'space-between'}}>
+                                <div className="col-12 col-sm-3 mt-25 ml-75">
+                                    <Button style={{width:"100%"}} className="btn-block" variant="primary">Transfer Fund</Button>{' '}
+                                </div>
+                            <div className="col-12 col-sm-3 mt-25 ml-75">
+                                <Button style={{width:"100%"}} className="btn-block" variant="light">Cancel</Button>{' '}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-sm-2"></div>
+                    <div className="col-12 col-sm-8" style={content}>
+                        <h5 style={{marginTop: '1rem'}}>Tips</h5>
+                        <p>
+                            1. Please do not include any information about BTC, ETH, USDT BNB and other digital asset names in the transfer notes to prevent payment from being intercepted or bank funds being frozen
+                        </p>
+                        <p>
+                            2. Please do not include any information about BTC, ETH, USDT BNB and other digital asset names in the transfer notes to prevent payment from being intercepted or bank funds being frozen
+                        </p>
+                        <p>
+                            3. Please do not include any information about BTC, ETH, USDT BNB and other digital asset names in the transfer notes to prevent payment from being intercepted or bank funds being frozen
+                        </p>
+                        <p>
+                            4. Please do not include any information about BTC, ETH, USDT BNB and other digital asset names in the transfer notes to prevent payment from being intercepted or bank funds being frozen
+                        </p>
                     </div>
                 </div>
             </div>
